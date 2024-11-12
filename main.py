7548f67403f663ea1d924320744765215e573750
wxn151 <wxn.npc@gmail.com>
2024-11-12 00:47:16 -0600

    add new game logic

--- main.py
+++ main.py
@@ -1,46 +1,39 @@
+
+####################
 import pygame
+import pytz
+####################
 import sys
 import time
 import random
 import threading
 import datetime
+import random
 
 pygame.init()
-
-intent = 10    #press-times
-nght_colour = (0, 0, 0)
-dy_colour = (255, 255, 255)
-screen = pygame.display.set_mode((205, 205))
-stats = f"Press {intent} times to open the egg. Press start (or any key)."
+thrw_ball = 0    #press-times
+default_colour = (0, 0, 0)
+screen = pygame.display.set_mode((210, 303))
+statement = f"Press a key when Pikachu blink to win. Press enter to start."
 running = True
-
 
 class Animate():
     def __init__(self):
-        self.sprites, self.finish = []
-        #self.mood = 'none'
-    def run(self):
+        self.sprites = []
+    def run(self, cheat):
         self.value = 0
         while self.value <= len(self.sprites):
             press = self.sprites[int(self.value)]
-            self.value += self.time
-            if (self.dayOrNight()):
-                self.mood = '_'
-            sprite = pygame.image.load(f'assets/{press}')
-            _rect = sprite.get_rect()
-            screen.blit(sprite, _rect)
-            pygame.display.update()
-    def close(self):
-        self.value = 0
-        while self.value <= len(self.finish):
-            ######  HOUR  ##########
-            if (self.value > 19):
+            ######  LOOP  #######
+            if (self.value < 0.17):
+                self.time = self._frame_time
+            elif (self.value < 0.2 and self.time < 0):
+                break
+            ######  FAKE OR NOT  #######
+            if (self.value > cheat):
                 self.time = -self.time
-            if (self.value < 3):
-                self.time = 0.01
-
             self.value += self.time
-            press = self.finish[int(self.value)]
+            screen.fill(default_colour)
             sprite = pygame.image.load(f'assets/{press}')
             _rect = sprite.get_rect()
             screen.blit(sprite, _rect)
@@ -50,63 +43,58 @@ class Animate():
 
 class Frame(Animate):
     def __init__(self):
-        self.sprites = ["frame0.jpg", "frame1.jpg", "frame2.jpg", "frame3.jpg", "frame4.jpg",]
-        self.finish = []
-        self.time = 0.01
-        self.delay = 0.0
+        self.sprites = ["frame0.gif", "frame1.gif", "frame2.gif", "frame3.gif"]
+        self.time = 0
+        self._frame_time = 0.00284
         self.value = 0
-        #self.date = datetime.datetime.now().strftime('%d/%m/%y %H:%M:%S')
-        self.hour = datetime.datetime.now().strftime('%H')
+        self.here = datetime.datetime.now().strftime('%d/%m/%y %H:%M')
+        self.japan = datetime.datetime.now(tz=pytz.timezone('Asia/Tokyo')).strftime('%d/%m/%y %H:%M')  # for Japan time zone
 
-    def __str__(self):
-        print(f"Your open the egg on {self.delay}")
+    def __str__(self, intent):
+        print(f"Your record is the {intent}")
 
-    def init_finish(self):
-        n = 0
-        while n < 20:
-            self.finish.append(f'end{str(n)}.jpg')
-            n+=1
-
-    def dayOrNight(self):
-        if (int(self.hour)>=18):
-            return False
-            #print('night')
-        else:
+    def init_blink(self):
+        if(self.value >= 3):
             return True
+        else:
+            return False
 
+    def JapanMood(self):
+        if (self.here == self.japan):
+            return True
+        else:
+            return False
+
+print(statement)
+screen.fill(default_colour)
+captured = 0
 frm = Frame()
-frm.init_finish()
-start_time = 0
-print(stats)
-
-if (frm.dayOrNight()):
-    screen.fill(nght_colour)
-else:
-    screen.fill(dy_colour)
-
-# game loop
 while running:
-    # for loop through the event queue
+    # for .... queue
+    rcrd = time.process_time()
+    screen.fill(default_colour)
+    sprite = pygame.image.load(f'assets/frame0.gif')
+    _rect = sprite.get_rect()
+    screen.blit(sprite, _rect)
+    pygame.display.update()
+    if (int(rcrd) % 2):
+        if (random.randint(0, 6) == 4):
+            frm.run(3.9)
+            if (captured < 0):
+                mood = frm.JapanMood()
+                if(mood):
+                    print('トドスのキャプチャ')
+                print("Pikachu was added to the pokedex!!!")
+            print(f'Your intents to capture the pokemon: {thrw_ball}')
+            break
+        else:
+            frm.run(2.9)
     for event in pygame.event.get():
         if event.type == pygame.KEYDOWN:
+            thrw_ball += 1
+            if (frm.init_blink()):
+                captured -= 1
 
-            if (intent >= 0):
-                frm.run()
-                intent -= 1
-                if (intent==9):
-                    start_time = time.time()
-
-            else:
-                if(intent == -1):
-                    intent -= 1
-                    end_time = time.time()
-                    rcrd = end_time - start_time
-                    frm.close()
-                print(f'Your record is the {rcrd}')
-
-        # Check for QUIT event
-        if event.type == pygame.QUIT:
-            running = False
 
 
 
